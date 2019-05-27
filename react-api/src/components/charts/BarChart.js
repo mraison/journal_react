5d9ecc6e9613c7e757acef5386a1f39ed7b1705e
raisonm <mattandthings@gmail.com>
2019-05-26 21:14:46 -0400

    I finally got this to load my data visualization successfully. And it looks like it's the general form I'll want to follow for the other pages (if they need data).

--- react-api/src/components/charts/BarChart.js
+++ react-api/src/components/charts/BarChart.js
@@ -8,59 +8,55 @@ class BarChart extends Component {
 	 * chartHeight int   optional
 	 *
 	 */
-	// constructor(props) {
-	//     super(props);
+	constructor(props) {
+	    super(props);
 
-	//     this.state = {
-	//       chartWidth: typeof props.chartWidth != 'undefined' ? props.chartWidth : 700,
-	//       chartHeight: typeof props.chartHeight != 'undefined' ? props.chartHeight : 300,
-	//       // chartID: typeof props.chartID != 'undefined' ? props.chartID : 1
-	//     };
-	//   }
+	    this.state = {
+	      chartWidth: typeof props.chartWidth != 'undefined' ? props.chartWidth : 700,
+	      chartHeight: typeof props.chartHeight != 'undefined' ? props.chartHeight : 300,
+	      // chartID: typeof props.chartID != 'undefined' ? props.chartID : 1
+	    };
+	  }
 
 	componentDidMount() {
-		// this.drawChart();
-		console.log('here here')
-		console.log(this.props)
+		this.drawChart();
 	}
 
-	// drawChart() {
-	// 	console.log('here here')
-	// 	console.log(this.props.chartData)
-	// 	const data = this.props.chartData;
-	// 	const w = this.state.chartWidth
-	// 	const h = this.state.chartHeight
-	// 	// create frame (I thiiiiiiiink...)
-	// 	const svg = d3.select("body")
-	// 	    .append("svg")
-	// 	    .attr("width", w)
-	// 	    .attr("height", h)
-	// 	    .style("margin-left", 100);
+	drawChart() {
+		const data = this.props.chartData;
+		const w = this.state.chartWidth
+		const h = this.state.chartHeight
+		// create frame (I thiiiiiiiink...)
+		const svg = d3.select("body")
+		    .append("svg")
+		    .attr("width", w)
+		    .attr("height", h)
+		    .style("margin-left", 100);
 	      
-	//       // place data    
-	//     svg.selectAll("rect")
-	//       .data(data)
-	//       .enter()
-	//       .append("rect")
-	//       .attr("x", (d, i) => i * 70)
-	//       .attr("y", (d, i) => h - 10 * d)
-	//       .attr("width", 65)
-	//       .attr("height", (d, i) => d * 10)
-	//       .attr("fill", "green")
+	      // place data    
+	    svg.selectAll("rect")
+	      .data(data)
+	      .enter()
+	      .append("rect")
+	      .attr("x", (d, i) => i * 70)
+	      .attr("y", (d, i) => h - 10 * d)
+	      .attr("width", 65)
+	      .attr("height", (d, i) => d * 10)
+	      .attr("fill", "green")
 
-	//       // label
-	//     svg.selectAll("text")
-	// 	  .data(data)
-	// 	  .enter()
-	// 	  .append("text")
-	// 	  .text((d) => d)
-	// 	  .attr("x", (d, i) => i * 70)
-	// 	  .attr("y", (d, i) => h - (10 * d) - 3)
+	      // label
+	    svg.selectAll("text")
+		  .data(data)
+		  .enter()
+		  .append("text")
+		  .text((d) => d)
+		  .attr("x", (d, i) => i * 70)
+		  .attr("y", (d, i) => h - (10 * d) - 3)
 
-	// }
+	}
 
 	render() {
-		return <div id={"#" + this.props.chartID}></div>
+		return (<div id={"#" + this.props.chartID}></div>)
 	}
 
 }
--- react-api/src/recordSearch.js
+++ react-api/src/recordSearch.js
@@ -1,50 +1,34 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import BarChart from './components/charts/BarChart'
 
 class userHome extends React.Component {
   constructor(props) {
       super(props);
+    }
 
+    componentDidMount() {
       // this.state = {}
       let j = {data: []}
       const url = `http://localhost:8080/users/${this.props.match.params.userID}/points`
       fetch(url, {method:'GET'})
         .then(response => response.json()) // convert reponse to json
         .then(data => {
-          const r = data.map(d => d.valueReal)
-          console.log(r)
-          j = {
-            data: r,
-            userID: this.props.match.params.userID
-          }
+          const d = data.map(j => j.valueReal)
+          // So my main data processing will need to go in here. same for the other modules.
+          ReactDOM.render(
+            <div>
+              <BarChart chartData={d} chartID={this.props.match.params.userID}/>
+            </div>, document.getElementById('searchResults')
+            )
         }); // set in state
-        console.log('constructor->')
-        console.log(j)
-        this.state = j
     }
-
-  // componentDidMount = () => {
-  //   const url = `http://localhost:8080/users/${this.props.match.params.userID}/points`
-  //   fetch(url, {method:'GET'})
-  //     .then(response => response.json()) // convert reponse to json
-  //     .then(data => {
-  //       const r = data.map(d => d.valueReal)
-  //       console.log(r)
-  //       this.setState({
-  //         data: r,
-  //         userID: this.props.match.params.userID
-  //       })
-  //     }); // set in state
-
-  // }
 
 // So just side note, things passed into components like that are considered props not state.
 // you have to set them in state.
   render() {
     return (
-      <div>
-        <BarChart chartData={this.state.data} chartID={this.state.userID}/>
-      </div>
+      <div id="searchResults"/>
     );
   }
 }
